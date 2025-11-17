321b958d2e3591d2d405aa945a31c2109dd9cbec
Pavel Kuliaka <pavel.kulyaka@yandex.ru>
2025-11-17 10:05:47 +0300

    Implemented a merge sort

---
+++ src/merge_sort.py
@@ -0,0 +1,27 @@
+def merge_sort(array: list) -> list:
+    if len(array) <= 1:
+        return array
+
+    mid = len(array) // 2
+    left = merge_sort(array[:mid])
+    right = merge_sort(array[mid:])
+
+    return merge(left, right)
+
+
+def merge(left: list, right: list) -> list:
+    result = []
+    i = j = 0
+
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+
+    result.extend(left[i:])
+    result.extend(right[j:])
+
+    return result
